6322017b40b2c6f828069f7007c47546af022130
Tom Insam <tom@movieos.org>
2014-08-26 23:40:59 -0700

    Sharing

--- app/src/main/java/org/movieos/proton/CorrectionActivity.java
+++ app/src/main/java/org/movieos/proton/CorrectionActivity.java
@@ -1,17 +1,31 @@
 package org.movieos.proton;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.ShareActionProvider;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 import butterknife.ButterKnife;
@@ -45,6 +59,7 @@ public class CorrectionActivity extends Activity implements SeekBar.OnSeekBarCha
     Bitmap mBitmap;
     CorrectionManager mCorrection;
     Mode mMode = Mode.ROTATE;
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +92,27 @@ public class CorrectionActivity extends Activity implements SeekBar.OnSeekBarCha
     protected void onResume() {
         super.onResume();
         updateImage();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.correction, menu);
+        MenuItem item = menu.findItem(R.id.action_share);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                save();
+                return true;
+            case R.id.action_share:
+                share();
+                return true;
+            default:
+                return super.onMenuItemSelected(featureId, item);
+        }
     }
 
     @OnClick(R.id.rotate_button)
@@ -163,5 +199,54 @@ public class CorrectionActivity extends Activity implements SeekBar.OnSeekBarCha
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
 
+    }
+
+    File getAlbumStorageDir(String albumName) {
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), albumName);
+        if (!file.mkdirs()) {
+            Log.e(TAG, "Directory not created");
+        }
+        return file;
+    }
+
+    void writeToFile(File file) {
+        Bitmap output = Bitmap.createBitmap(mBitmap.getWidth(), mBitmap.getHeight(), mBitmap.getConfig());
+        Canvas canvas = new Canvas(output);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        canvas.drawBitmap(mBitmap, mCorrection.getMatrix(mBitmap), paint);
+        try {
+            FileOutputStream stream = new FileOutputStream(file);
+            output.compress(Bitmap.CompressFormat.JPEG, 80, stream);
+            stream.close();
+        } catch (IOException e) {
+            Log.e(TAG, "java.io.IOException", e);
+            Toast.makeText(this, R.string.write_error, Toast.LENGTH_LONG).show();
+            return;
+        } finally {
+            output.recycle();
+        }
+    }
+
+    void save() {
+        File album = getAlbumStorageDir(getString(R.string.app_name));
+        writeToFile(new File(album, new Date().toString() + ".jpg"));
+        Toast.makeText(this, R.string.save_complete, Toast.LENGTH_LONG).show();
+    }
+
+    void share() {
+        File folder = new File(getExternalFilesDir("share"), "temp");
+        folder.mkdirs();
+        for (String filename : folder.list()) {
+            new File(folder, filename).delete();
+        }
+        File temp = new File(folder, new Date().toString() + ".jpg");
+        writeToFile(temp);
+        Log.i(TAG, "tmep is " + temp.toURI());
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        intent.setType("image/jpeg");
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(temp));
+        startActivity(Intent.createChooser(intent, getString(R.string.share_title)));
     }
 }
