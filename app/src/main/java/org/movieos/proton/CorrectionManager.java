d5c560c04c9e26b59fc7ab000c84e5ac71670635
Tom Insam <tom@movieos.org>
2014-09-13 21:43:38 -0700

    implement vskew

--- app/src/main/java/org/movieos/proton/CorrectionManager.java
+++ app/src/main/java/org/movieos/proton/CorrectionManager.java
@@ -14,27 +14,40 @@ public class CorrectionManager {
         Matrix matrix = new Matrix();
 
         float hskew = (float) (mHorizontalSkew * source.getWidth() * 0.02f);
+        float vskew = (float) (mVerticalSkew * source.getWidth() * 0.02f);
         float[] bounds = new float[] {
                 0, 0,
                 source.getWidth(), 0,
                 0, source.getHeight(),
                 source.getWidth(), source.getHeight()
         };
-        float[] dest;
+        float[] dest = new float[] {
+                0, 0,
+                source.getWidth(), 0,
+                0, source.getHeight(),
+                source.getWidth(), source.getHeight()
+        };
         if (hskew > 0) {
-            dest = new float[] {
-                    0, 0,
-                    source.getWidth(), 0,
-                    -hskew, source.getHeight() + hskew,
-                    source.getWidth() + hskew, source.getHeight() + hskew
-            };
+            dest[4] -= hskew;
+            dest[5] += hskew;
+            dest[6] += hskew;
+            dest[7] += hskew;
         } else {
-            dest = new float[] {
-                    hskew, hskew,
-                    source.getWidth() - hskew, hskew,
-                    0, source.getHeight(),
-                    source.getWidth(), source.getHeight()
-            };
+            dest[0] += hskew;
+            dest[1] += hskew;
+            dest[2] -= hskew;
+            dest[3] += hskew;
+        }
+        if (vskew > 0) {
+            dest[0] -= vskew;
+            dest[1] -= vskew;
+            dest[4] -= vskew;
+            dest[5] += vskew;
+        } else {
+            dest[2] -= vskew;
+            dest[3] += vskew;
+            dest[6] -= vskew;
+            dest[7] -= vskew;
         }
         matrix.setPolyToPoly(bounds, 0, dest, 0, 4);
 
